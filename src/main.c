d0fcb7d4203333476266f6f708a2dede7ccfd8ed
Steven Benner <admin@stevenbenner.com>
2022-02-18 05:38:16 -0800

    Create basic command line interface handling (--version)

--- src/main.c
+++ src/main.c
@@ -32,6 +32,10 @@
 #include "settings.h"
 #include "window.h"
 
+static struct app_options_t {
+	gboolean show_version;
+} app_options;
+
 static void init_i18n(void)
 {
 #ifdef ENABLE_NLS
@@ -42,8 +46,35 @@ static void init_i18n(void)
 #endif
 }
 
-static void on_activate_app(GtkApplication *app, gpointer user_data)
+void init_cli_options(GApplication *app, struct app_options_t *options)
 {
+	const GOptionEntry cli_options[] = {
+		{
+			.long_name = "version",
+			.short_name = 'v',
+			.flags = G_OPTION_FLAG_NONE,
+			.arg = G_OPTION_ARG_NONE,
+			.arg_data = &(options->show_version),
+			/* l10n: cli option descriptions */
+			.description = N_("Show version information"),
+			.arg_description = NULL
+		},
+		{ NULL }
+	};
+
+	g_application_add_main_option_entries(G_APPLICATION(app), cli_options);
+}
+
+static void on_activate_app(GtkApplication *app, struct app_options_t *options)
+{
+	if (options->show_version == TRUE) {
+		/* l10n: cli --version output */
+		g_print(_("PacFinder - version %s\n\n"), VERSION);
+		g_print(_("Copyright %s Steven Benner\n\n"), COPYRIGHT_YEAR);
+		g_print(_("Report bugs to <%s>.\n"), PACKAGE_BUGREPORT);
+		return;
+	}
+
 	create_app_window(app);
 	initialize_main_window();
 }
@@ -67,7 +98,8 @@ int main(int argc, char **argv)
 
 	/* launch gtk application */
 	app = gtk_application_new(APPLICATION_ID, G_APPLICATION_FLAGS_NONE);
-	g_signal_connect(app, "activate", G_CALLBACK(on_activate_app), NULL);
+	g_signal_connect(app, "activate", G_CALLBACK(on_activate_app), &app_options);
+	init_cli_options(G_APPLICATION(app), &app_options);
 	status = g_application_run(G_APPLICATION(app), argc, argv);
 	g_object_unref(app);
 
